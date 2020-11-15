d9d80c5835f241b932c3493519376b2af6b71416
agmejias011 <agmejias011@hotmail.com>
2020-11-15 14:51:19 -0500

    Revert "After merged user issues: added functionality for researcher users and research institution: services, controllers, repo"

--- src/main/java/edu/fiu/ffqr/models/Parent.java
+++ src/main/java/edu/fiu/ffqr/models/Parent.java
@@ -17,16 +17,15 @@ public class Parent extends User implements Serializable{
 
     @JsonProperty("assignedclinic")
     private String assignedclinic;
-    @JsonProperty("assignedclinician")
-    private String assignedclinician;    
+	@JsonProperty("assignedclinician")
+    private String assignedclinician;
     @JsonProperty("childrennames")
     private ArrayList<String> childrennames = new ArrayList<String>();
 
 	public Parent() {}
 	
-    public Parent(String userId, String username, String userpassword, String usertype, 
-            String firstname, String lastname, String assignedclinic,
-            String assignedclinician, ArrayList<String> childrennames, boolean isactive){
+    public Parent(String userId, String username, String userpassword, String usertype, String firstname, String lastname, String assignedclinic,
+    String assignedclinician, ArrayList<String> childrennames, boolean isactive){
         this.userId = userId;
 		this.username = username;
         this.userpassword = userpassword;
@@ -37,7 +36,6 @@ public class Parent extends User implements Serializable{
         this.assignedclinician = assignedclinician;
         this.childrennames = childrennames;
         this.isactive = isactive;
-
     }
 
     public String getAssignedclinic() {
@@ -53,7 +51,6 @@ public class Parent extends User implements Serializable{
     public void setAssignedclinician(String assignedclinician) {
         this.assignedclinician = assignedclinician;
     }
-
 
     public ArrayList<String> getChildrenNames() {
         return this.childrennames;
